d53526309b5c7a730c91918d52c0ee4ae29d743c
Pedro Torres <45828743+phesgot@users.noreply.github.com>
2025-04-17 13:58:53 -0300

    Prepara o layout da aplicação

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,7 @@
 <?php
 
-use App\Models\User;
-use Illuminate\Mail\Message;
-use Illuminate\Support\Facades\Mail;
+
 use Illuminate\Support\Facades\Route;
 
-Route::get('/email', function () {
-    Mail::raw("Menssagem de teste de RH MANGNT", function(Message $message){
-        $message->to('teste@gmail.com')
-        ->subject('Bem-vindo ao RH MANGNT')
-        ->from('rh@rhmangnt.com');
-    });
-    echo "Email enviado com sucesso.";
-});
+Route::view('/login', 'auth.login');
 
-
-Route::get('/admin', function(){
-    $admin = User::with('detail', 'department')->find(1);
-
-    dd($admin->toArray());
-
-});

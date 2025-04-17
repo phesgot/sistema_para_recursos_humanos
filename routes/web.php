866acdd2f2c0ae8c731103139d2478a2536d2699
Pedro Torres <45828743+phesgot@users.noreply.github.com>
2025-04-17 10:51:50 -0300

    Merge feature/MailHog

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use Illuminate\Mail\Message;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    echo "RH MANGNT";
+Route::get('/email', function () {
+    Mail::raw("Menssagem de teste de RH MANGNT", function(Message $message){
+        $message->to('teste@gmail.com')
+        ->subject('Bem-vindo ao RH MANGNT')
+        ->from('rh@rhmangnt.com');
+    });
+    echo "Email enviado com sucesso.";
 });

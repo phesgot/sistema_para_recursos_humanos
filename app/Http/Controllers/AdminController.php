e47b0ec4d09ce66d016fa0f44a4ad54099d94856
Pedro Torres <45828743+phesgot@users.noreply.github.com>
2025-04-24 01:12:04 -0300

    Prepara os dados a ser apresentados na tela home do admin

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Contracts\View\View;
 use Illuminate\Support\Facades\Auth;
 
@@ -11,7 +12,51 @@ class AdminController extends Controller
     {
         Auth::user()->can('admin') ?: abort(403, 'Você não tem permissão para acessar esta página.');
 
+        // colleact all information about the organization
+        $data = [];
+
+        // get total number of colaborators (deletes_at is null)
+        $data['total_colaborators'] = User::whereNull('deleted_at')->count();
+
+        // total colaborators deleted
+        $data['total_colaborators_deleted'] = User::onlyTrashed()->count();
+
+        // total salary for all colaborators
+        $data['total_salary'] = User::withoutTrashed()
+            ->with('detail')
+            ->get()->sum(function ($colaborator) {
+                return $colaborator->detail->salary;
+            });
+
+            $data['total_salary'] = 'R$ ' . number_format($data['total_salary'], 2, ',', '.');
+
+        // total colaborators by department
+        $data['total_colaborators_per_department'] = User::withoutTrashed()
+            ->with('department')
+            ->get()
+            ->groupBy('department_id')
+            ->map(function ($department) {
+                return [
+                    'department' => $department->first()->department->name ?? "-",
+                    'total' =>  $department->count()
+                ];
+            });
+
+        // total salary by department
+        $data['total_salary_by_department'] = User::withoutTrashed()
+            ->with('department', 'detail')
+            ->get()
+            ->groupBy('department_id')
+            ->map(function ($department) {
+                return [
+                    'department' => $department->first()->department->name ?? "-",
+                    'total' => $department->sum(function ($colaborator) {
+                        return $colaborator->detail->salary;
+                    })
+                ];
+            });
+
         // display admin home page
-        return view('home');
+        return view('home', compact('data'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,6 +3,10 @@
     <div class="w-100 p-4">
         <h3>Home</h3>
         <hr>
+
+        <x-info-title-value item-title="Total de colaboradores" :item-value="$data['total_colaborators']" />
+        <x-info-title-value item-title="Total de colaboradores deletado" :item-value="$data['total_colaborators_deleted']" />
+        <x-info-title-value item-title="Somatório dos salários" :item-value="$data['total_salary']" />
     </div>
 
 </x-layout-app>

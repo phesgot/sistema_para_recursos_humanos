a5164ccdd6981596fde096d108d0436c7e7dadb5
Pedro Torres <45828743+phesgot@users.noreply.github.com>
2025-05-04 11:11:47 -0300

    Testar processo de login com sucesso para o perfil de admin

--- tests/Feature/AuthTest.php
+++ tests/Feature/AuthTest.php
@@ -1,11 +1,13 @@
 <?php
 
-it('display the login page when not logged in', function(){
+use App\Models\User;
+
+it('display the login page when not logged in', function () {
 
     // verifica, no contexto do Fortify, se ao entrar na página inicial, vai ser
     // redirecionado para a página de login.
     $result = $this->get('/')->assertRedirect('/login');
-    
+
     // verifica se o resultado é o 302
     expect($result->status())->toBe(302);
 
@@ -14,10 +16,35 @@ it('display the login page when not logged in', function(){
 
     // verifica se a página de login contém o texto "Esqueceu a sua senha?"
     expect($this->get('/login')->content())->toContain("Esqueceu a sua senha?");
-
 });
 
-it('Display the recover password page correctly', function(){
+it('Display the recover password page correctly', function () {
     expect($this->get('/forgot-password')->status())->toBe(200);
     expect($this->get('/forgot-password')->content())->toContain("Já sei a minha senha?");
+});
+
+it('Test if an admin user can login with siccess', function () {
+
+    // criar um admin
+    User::insert([
+        'department_id' => 1,
+        'name' => 'Administrador',
+        'email' => 'admin@rhmangnt.com',
+        'email_verified_at' => now(),
+        'password' => bcrypt('Aa123456'),
+        'role' => 'admin',
+        'permissions' => '["admin"]',
+        'created_at' => now(),
+        'updated_at' => now(),
+    ]);
+
+    // login com admin criado
+    $result = $this->post('/login', [
+        "email" => 'admin@rhmangnt.com',
+        "password" => 'Aa123456',
+    ]);
+
+    // verifica se o login foi feito com sucesso
+    expect($result->status())->toBe(302);
+    expect($result->assertRedirect('/home'));
 });
